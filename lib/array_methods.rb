990db5381f783bc0dc39a5dd3d29442a93760ff2
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-16 19:44:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 
 def find_element_index(array, value_to_find)
-  p array.index(value_to_find)
+  # p array.index(value_to_find)
 end
 
 def find_max_value(array)
@@ -9,5 +9,6 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  p array.min 
+  # p array.min 
+  
 end
