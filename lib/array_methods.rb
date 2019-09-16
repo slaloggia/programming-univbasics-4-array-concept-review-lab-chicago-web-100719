3ca798be64acb7aae8996911c62e43e4ef3bd56d
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-16 20:18:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,6 +17,14 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # p array.min 
+  min = array[0]
+  array.length.times { |i|
+  puts array[i] 
+  if array[i] < max
+    max = array[i]
+  end
+  }
+  
+  p max
   
 end
