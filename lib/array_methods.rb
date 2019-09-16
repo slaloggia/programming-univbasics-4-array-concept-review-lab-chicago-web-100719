c21fd359edfca73e814d7bd4c8a5dbaac1717d24
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-16 19:15:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,20 @@
+def compare_values(array)
+    y = 0 
+    array.length.times do
+    array[index] >= array[y]
+    y += 1
+    end
+  end
+  
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  p array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  index = 0 
+  
+  
+  
 end
 
 def find_min_value(array)
