21d0ffb8cc61583349c546ef30862a0b861368b4
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-16 19:27:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
-def compare_values(array)
-    y = 0 
-    array.length.times do
-    array[index] >= array[y]
-    y += 1
-    end
-  end
-  
+
 def find_element_index(array, value_to_find)
   p array.index(value_to_find)
 end
 
 def find_max_value(array)
   index = 0 
+  
+  def compare_values(array)
+    y = 0 
+    array.length.times do
+    array[index] >= array[y]
+    y += 1
+    end
   
   while array[index] do
     compare_values(array)
