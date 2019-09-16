4f64910eb56d340fc254c035a77939c6ec82fa6f
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-16 19:55:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,9 +7,12 @@ def find_max_value(array)
   # would acutally use p array.max but that's clearly not what you want
   max = 0 
   index = 0 
-  
-  if array[index] > max
-    max = array[index]
+  array.each do
+    if array[index] > max
+      max = array[index]
+    end
+    
+    puts max
   
 end
 
